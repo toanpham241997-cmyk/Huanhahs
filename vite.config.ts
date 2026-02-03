15ab142bd85678a720a0a6375e0ca46a4878199c
toanpham241997-cmyk <toanpham241997@gmail.com>
2026-02-04 00:57:23 +0700

    Update Vite config to specify build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/Huanhahs/",
   plugins: [react()],
+  root: "client",
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
 });
